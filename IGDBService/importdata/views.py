be7bd4d6c1d1e8a8bbcb9d22b59c5815f44baff6
Arthur Rodrigues <arthurarp97@gmail.com>
2018-09-05 20:59:25 -0300

    [FIX] Fixed identation and method calling

--- IGDBService/importdata/views.py
+++ IGDBService/importdata/views.py
@@ -8,18 +8,15 @@ from .serializers import IGDBGameSerializer
 
 
 class IgDBView(APIView):
-
     '''
-            View that calls IGDB API
-            and return some relevant
-            information about a game
-            and filter for Null value
+        View that calls IGDB API
+        and return some relevant
+        information about a game
+        and filter for Null value
     '''
-
     def get(self, request, format=None):
-
         header = {'user-key': '9c3039ea4ad4cb83bfb126100764c483',
-                  'Accept': 'application/json'}
+        'Accept': 'application/json'}
         url = 'https://api-endpoint.igdb.com/games/?fields=id,name,hypes,popularity,aggregated_rating,time_to_beat,genres&filter[rating][gte]=60&order=popularity:desc&limit=50&offset=0'
         data = requests.get(url, headers=header)
         ndata = data.json()
@@ -27,18 +24,18 @@ class IgDBView(APIView):
         for gamedata in ndata:
             filtered_data = self.filter_data(gamedata)
             self.save_game(filtered_data)
-            self.get_genre(gamedata['genres'])
+            self.get_genres(gamedata['genres'])
 
         games = IGDBGame.objects.all()
         for game in games:
-        	print('------------')
-        	print(game.id)
-        	print(game.name)
-        	print(game.hypes)
-        	print(game.popularity)
-        	print(game.aggregated_rating)
-        	print(game.time_to_beat)
-        	print('------------')
+            print('------------')
+            print(game.id)
+            print(game.name)
+            print(game.hypes)
+            print(game.popularity)
+            print(game.aggregated_rating)
+            print(game.time_to_beat)
+            print('------------')
 
         return Response(data=ndata)
 
@@ -78,55 +75,56 @@ class IgDBView(APIView):
             time_to_beat = None
 
         if 'genres' in gamedata:
-           	genres = gamedata['genres']
+            genres = gamedata['genres']
         else:
             genres = None
 
         filtered_data = {
-            'id': id,
-            'name': name,
-            'hypes': hypes,
-            'popularity': popularity,
-            'aggregated_rating': aggregated_rating,
-            'time_to_beat': time_to_beat,
-            'genres': genres
+        'id': id,
+        'name': name,
+        'hypes': hypes,
+        'popularity': popularity,
+        'aggregated_rating': aggregated_rating,
+        'time_to_beat': time_to_beat,
+        'genres': genres
         }
 
         return filtered_data
 
     def save_game(self, filtered_data):
-    	new_game = IGDBGame(
-    		id = filtered_data['id'],
-    		name = filtered_data['name'],
-    		hypes = filtered_data['hypes'],
-    		popularity = filtered_data['popularity'],
-    		aggregated_rating = filtered_data['aggregated_rating'],
-    		time_to_beat = filtered_data['time_to_beat']
+        new_game = IGDBGame(
+        id = filtered_data['id'],
+        name = filtered_data['name'],
+        hypes = filtered_data['hypes'],
+        popularity = filtered_data['popularity'],
+        aggregated_rating = filtered_data['aggregated_rating'],
+        time_to_beat = filtered_data['time_to_beat']
 
-    	)
+        )
 
-    	new_game.save()
-    	self.get_genres(genres_id_list)
+        new_game.save()
+        self.get_genres(filtered_data['genres'])
 
-    	print('o jogo salvou ' + new_game.name)
+        print('o jogo salvou ' + new_game.name)
 
     def get_genres(self, genres_id_list):
-    	genres = []
+        genres = []
 
-    	for genre_id in genres_id_list:
-    		url = 'https://api-endpoint.igdb.com/genres/{}?fields=name'.format(genre_id)
-    		header = {'user-key': '9c3039ea4ad4cb83bfb126100764c483',
-                  'Accept': 'application/json'}
-        
-       	 	data = requests.get(url, headers=header)
-        	ndata = data.json()
+        for genre_id in genres_id_list:
+            url = 'https://api-endpoint.igdb.com/genres/{}?fields=name'.format(genre_id)
+            header = {'user-key': '9c3039ea4ad4cb83bfb126100764c483',
+            'Accept': 'application/json'}
 
-        	genre = Genre(
-        		id = ndata['id'],
-        		name = ndata['name']
-			)
+        data = requests.get(url, headers=header)
+        ndata = data.json()
 
-        	genres.append(genre)
+
+        genre = Genre(
+        id = ndata[0]['id'],
+        name = ndata[0]['name']
+        )
+
+        genres.append(genre)
 
         return genres
 
