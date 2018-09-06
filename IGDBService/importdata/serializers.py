35d52bd2fff82bc3a5d234896e6a8244899417af
Arthur Rodrigues <arthurarp97@gmail.com>
2018-09-06 13:44:17 -0300

    [ADD] fga-eps-mds/2018.2-GamesBI#90 Imported external attribute
    Co-authored-by: Andre Eduardo <andre.eduardo11@hotmail.com>

--- IGDBService/importdata/serializers.py
+++ IGDBService/importdata/serializers.py
@@ -1,12 +1,13 @@
 from .models import IGDBGame, Genre
 from rest_framework import serializers
 
-class IGDBGameSerializer(serializers.ModelSerializer):
+class GameSerializer(serializers.ModelSerializer):
 
 	class Meta:
 
 		model = IGDBGame
 		fields = '__all__'
+
 
 class GenreSerializer(serializers.ModelSerializer):
 
@@ -14,3 +15,16 @@ class GenreSerializer(serializers.ModelSerializer):
 
 		model = Genre
 		fields = '__all__'
+
+
+class GameSerializerList(serializers.ModelSerializer):
+	genres = GenreSerializer(
+		many=True
+	)
+	
+	class Meta:
+
+		model = Genre
+		fields = '__all__'
+
+
