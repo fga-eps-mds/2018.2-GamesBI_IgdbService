a8dcd9587a74b58263a21406e17afaa563eb1513
Andre <aandre.eduardo11@hotmail.com>
2018-09-10 13:40:23 -0300

    [ADD] fga-eps-mds/2018.2-GamesBI#100 Add new keys
    Signed-off-by: Andre <andre.eduardo11@hotmail.com>
    Co-authored-by: Arthur <arthurarp97@gmail.com>

--- IGDBService/importdata/views.py
+++ IGDBService/importdata/views.py
@@ -1,10 +1,12 @@
 import requests
+import os
 
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import IGDBGame, Genre
 from django.shortcuts import render
+
 
 
 class IgDBView(APIView):
@@ -16,27 +18,33 @@ class IgDBView(APIView):
     '''
 
     def get(self, request, format=None):
-        IGDBGame.objects.all().delete()
-        header = {'user-key': '8ac128e6b3e9709134ad83ac072d0d59',
-        'Accept': 'application/json'}
-        url = 'https://api-endpoint.igdb.com/games/?fields=id,name,hypes,popularity,aggregated_rating,time_to_beat,external,genres&filter[rating][gte]=60&order=popularity:desc&limit=1&scroll=1'
-        data = requests.get(url, headers=header)
-        max_result = int(data.headers['x-count']) # retorna o a quantidade de itens do endpoint
+
+        with open(os.path.abspath('IGDBService/importdata/keys1.txt'), 'r') as keys1:
+            KEY_1 = keys1.read().rstrip()
+
+        with open(os.path.abspath('IGDBService/importdata/keys2.txt'), 'r') as keys2:
+            KEY_2 = keys2.read().rstrip()
+
+        with open(os.path.abspath('IGDBService/importdata/keys3.txt'), 'r') as keys3:
+            KEY_3 = keys3.read().rstrip()
+
+        #max_result = int(data.headers['x-count']) # retorna o a quantidade de itens do endpoint
 
         max_result = 50 # apenas para fins de teste  , para nao estourar o limite da user_key (retornara apenas 50 games)
 
         for page in range(0, max_result, 50): #cada solicitaçao retona no maximo 50 valores, assim o for pega todos os itens do endpoint
-            url = 'https://api-endpoint.igdb.com/games/?fields=id,name,hypes,popularity,aggregated_rating,time_to_beat,external,genres&filter[rating][gte]=60&order=popularity:desc&limit=5&offset='+str(page)
+
+
+            header = {'user-key': KEY_1,
+            'Accept': 'application/json'}
+
+            url = 'https://api-endpoint.igdb.com/games/?fields=id,name,hypes,popularity,aggregated_rating,time_to_beat,external,genres&filter[rating][gte]=60&order=popularity:desc&limit=50&offset='+str(page)
             data = requests.get(url, headers=header)
             ndata = data.json()
 
             for gamedata in ndata:
                 filtered_data = self.filter_data(gamedata)
-                self.save_game(filtered_data)
-                self.get_genres(gamedata['genres'])
-
-            games = IGDBGame.objects.all()
-
+                self.save_game(filtered_data, KEY_1)
 
         return Response(data=ndata)
 
@@ -100,7 +108,7 @@ class IgDBView(APIView):
 
         return filtered_data
 
-    def save_game(self, filtered_data):
+    def save_game(self, filtered_data, key):
         new_game = IGDBGame(
             id = filtered_data['id'],
             name = filtered_data['name'],
@@ -113,14 +121,19 @@ class IgDBView(APIView):
         )
 
         new_game.save()
-        genres = self.get_genres(filtered_data['genres'])
+        genres = self.get_genres(filtered_data['genres'], key)
+
+
 
         for genre in genres:
             new_game.genres.add(genre)
             new_game.save()
 
+        print(new_game.name)
+        for genre in new_game.genres.all():
+            print(genre.name)
 
-    def get_genres(self, genres_id_list):
+    def get_genres(self, genres_id_list, key):
 
         genres = []
 
@@ -137,7 +150,7 @@ class IgDBView(APIView):
                 genres_id_list[0]
                 )
 
-        header = {'user-key': '8ac128e6b3e9709134ad83ac072d0d59',
+        header = {'user-key': key,
         'Accept': 'application/json'}
 
         data = requests.get(url, headers=header)
@@ -146,8 +159,8 @@ class IgDBView(APIView):
         for genre in ndata:
 
             genre = Genre(
-                id = ndata[0]['id'],
-                name = ndata[0]['name']
+                id = genre['id'],
+                name = genre['name']
             )
             genre.save()
 
