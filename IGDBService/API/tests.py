8ad220440f5ea414ca7b6abe9da750e6d7eb5c67
Bruno Rodrigues <bruesmanbruesman@hotmail.com>
2018-09-13 13:33:17 -0300

    [ADD] fga-eps-mds/2018.2-GamesBI#97 Added new test to check steam response data
    Co-authored-by: Augusto Moreno Vilarins <augusto.vilarins@gmail.com>

--- IGDBService/API/tests.py
+++ IGDBService/API/tests.py
@@ -1,38 +1,59 @@
 from django.urls import reverse
 
 from rest_framework import status
-from rest_framework.test import APITestCase
+from django.urls import include, path, reverse
+from rest_framework.test import APITestCase, URLPatternsTestCase
 
 from model_mommy import mommy
 from IGDBService.importdata.models import IGDBGame
 
 
-class EndpointsTestCase(APITestCase):
+class EndpointsTestCase(APITestCase, URLPatternsTestCase):
+
+	urlpatterns = [
+        path('api/', include('IGDBService.API.urls')),
+    ]
 
 	def setUp(self):
 		"""
 			Set up will run before any test
 		"""
-		self.game = mommy.make(
+		self.game_steam = mommy.make(
 			IGDBGame,
             id=0,
-            name="JogoTeste",
+            name="JogoTesteSteam",
+            steam=123,
+        )
+
+		self.game = mommy.make(
+			IGDBGame,
+            id=1,
+            name="JogoTesteNaoSteam",
             steam=None,
         )
 
-		self.steam_endpoint = 'http://igdbweb:8000/api/get_igdb_games_list/id_steam'
+		self.steam_endpoint = reverse('get_igdb_games_id_steam_list')
 
 	def tearDown(self):
 		IGDBGame.objects.all().delete()
 
 	def test_status_steam_endpoint(self):
 
+		'''
+			Check endpoint status
+		'''
+
 		response = self.client.get(self.steam_endpoint)
 		self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 	def test_response_steam_endpoint(self):
 
-		response = self.client.get(self.steam_endpoint)
-		data = response.json()
+		'''
+			Test if steam endpoint
+			is returning only steam game
+		'''
+
+		response = self.client.get(self.steam_endpoint, format='json')
 
 		self.assertNotEqual(IGDBGame.objects.all().count(), 0)
+		self.assertEqual(len(response.data),1)
